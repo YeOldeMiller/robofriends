f88cee85360611540c27a9e245c8b49bfec9fc67
alt.muhle@gmail.com <36291373+YeOldeMiller@users.noreply.github.com>
2019-01-24 13:33:45 +0300

    Updates

---
+++ precache-manifest.cb1d1be60141351b9ab70b909e04f38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bef4c17df7fe7080bbd",
+    "url": "/robofriends/static/css/main.9938aa15.chunk.css"
+  },
+  {
+    "revision": "1bef4c17df7fe7080bbd",
+    "url": "/robofriends/static/js/main.1bef4c17.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/robofriends/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/robofriends/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "57f75a16d74026176bee967ffed2cd2a",
+    "url": "/robofriends/index.html"
+  }
+];
